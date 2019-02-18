eb61e8aec2113c8c37c0b8c96e79e4760426c9a4
Melanie Gonzalez <mnetter@utexas.edu>
2019-02-17 23:06:57 -0600

    fixed typos in RunEverything

--- RunEverything.R
+++ RunEverything.R
@@ -3,13 +3,13 @@ source("analysis/fig1_top50overall.R")
 fig1
 
 #run Fig 2
-source("analysis/fig1_top50overall.R")
+source("analysis/fig2_comparerawfreq_tannins.R")
 fig2
 
 #run Fig 3
-source("analysis/fig1_top50overall.R")
+source("analysis/fig3_comparerelfreq_tannins.R")
 fig3
 
 #run Fig 4
-source("analysis/fig1_top50overall.R")
+source("analysis/fig4_comparekeyness.R")
 fig4
